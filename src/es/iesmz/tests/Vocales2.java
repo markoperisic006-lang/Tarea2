ac6a307ce16b27b267b7717f158a679fcdf044cc
markoperisic006-lang <markoperisic006@gmail.com>
2026-05-28 13:24:24 +0200

    Ejercicio 3

---
+++ src/es/iesmz/tests/Vocales2.java
@@ -0,0 +1,11 @@
+package es.iesmz.tests;
+
+public class Vocales2 {
+
+    public static String sinvocales(String cadena) {
+        if (cadena == null) {
+            return null;
+        }
+        return cadena.replaceAll("[aeiouAEIOUáéíóúÁÉÍÓÚüÜ]", "");
+    }
+}
